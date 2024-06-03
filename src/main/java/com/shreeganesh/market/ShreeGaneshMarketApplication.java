8e2c9c06e26af3351b76c0fa5a3adea5785032d7
kunal dnagade <kunaldangade13@gmail.com>
2024-06-03 17:48:10 +0530

    initial spring boot stater project setup

---
+++ src/main/java/com/shreeganesh/market/ShreeGaneshMarketApplication.java
@@ -0,0 +1,13 @@
+package com.shreeganesh.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShreeGaneshMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShreeGaneshMarketApplication.class, args);
+	}
+
+}
